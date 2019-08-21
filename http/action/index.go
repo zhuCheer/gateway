a866421e76d522eafe0f9acc1443b53d0e74fcfa
zhuCheer <zhu.cheer@qq.com>
2019-08-21 17:22:35 +0800

    节点刷新

--- http/action/index.go
+++ http/action/index.go
@@ -3,6 +3,9 @@ package action
 import (
 	"fmt"
 	"github.com/zhuCheer/gateway/http/context"
+	"github.com/zhuCheer/gateway/model"
+	"github.com/zhuCheer/gateway/proxy"
+	"log"
 )
 
 func Ping(ctx *context.Context) {
@@ -13,6 +16,18 @@ func Ping(ctx *context.Context) {
 
 // 刷新节点信息
 func ReloadNodes(ctx *context.Context) {
-	fmt.Println("done1")
+
+	list := model.SitesDB.GetAll()
+	for _, item := range list {
+		log.Printf("fresh proxy %s://%s %s", item.Scheme, item.Domain, item.Balance)
+		proxy.ProxySrv.FlushProxy(item.Domain)
+		proxy.ProxySrv.RegistSite(item.Domain, item.Balance, item.Scheme)
+		nodes := model.NodesDB.QueryListBySiteId(item.ID)
+		for _, node := range nodes {
+			proxy.ProxySrv.AddAddr(item.Domain, node.Addr, node.Weight)
+			log.Printf("└ proxy node %s %d", node.Addr, node.Weight)
+		}
+	}
+
 	ctx.Response().Write([]byte("done"))
 }
